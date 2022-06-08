ba66e52e5bd605d62e56919df7945fce1ec2572b
Zachary Lim <zachlim@ischool.berkeley.edu>
2022-06-08 08:33:20 +0800

    Cache file clearing

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ app.config['SESSION_TYPE'] = 'filesystem'
 app.config['SESSION_FILE_DIR'] = './.flask_session/'
 Session(app)
 
-# caches_folder = './.spotify_caches/'
+caches_folder = './.spotify_caches/'
 # if not os.path.exists(caches_folder):
 #     os.makedirs(caches_folder)
 
